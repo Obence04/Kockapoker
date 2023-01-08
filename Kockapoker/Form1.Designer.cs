907ef344361bb79db68f3871287eb95e3a64edab
Bence Oláh <olah.bence@verebelyszki.hu>
2023-01-08 17:30:48 +0100

    Elkészült a program többi része

--- Kockapoker/Form1.Designer.cs
+++ Kockapoker/Form1.Designer.cs
@@ -28,6 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.BTN_Dobas = new System.Windows.Forms.Button();
             this.LB_dobott_text = new System.Windows.Forms.Label();
             this.LB_Dobott1 = new System.Windows.Forms.Label();
@@ -44,136 +49,162 @@
             this.LB_Dobasok = new System.Windows.Forms.Label();
             this.DGV_Tablazat = new System.Windows.Forms.DataGridView();
             this.Kat = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Pont = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pont = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.LB_Korok = new System.Windows.Forms.Label();
+            this.LB_Vegeredmeny = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Tablazat)).BeginInit();
             this.SuspendLayout();
             // 
             // BTN_Dobas
             // 
+            this.BTN_Dobas.BackColor = System.Drawing.Color.SeaGreen;
+            this.BTN_Dobas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BTN_Dobas.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.BTN_Dobas.Location = new System.Drawing.Point(12, 12);
             this.BTN_Dobas.Name = "BTN_Dobas";
-            this.BTN_Dobas.Size = new System.Drawing.Size(75, 75);
+            this.BTN_Dobas.Size = new System.Drawing.Size(150, 75);
             this.BTN_Dobas.TabIndex = 0;
             this.BTN_Dobas.Text = "Dobás";
-            this.BTN_Dobas.UseVisualStyleBackColor = true;
+            this.BTN_Dobas.UseVisualStyleBackColor = false;
             this.BTN_Dobas.Click += new System.EventHandler(this.BTN_Dobas_Click);
             // 
             // LB_dobott_text
             // 
             this.LB_dobott_text.AutoSize = true;
-            this.LB_dobott_text.Location = new System.Drawing.Point(96, 16);
+            this.LB_dobott_text.BackColor = System.Drawing.Color.Transparent;
+            this.LB_dobott_text.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LB_dobott_text.Location = new System.Drawing.Point(10, 140);
             this.LB_dobott_text.Name = "LB_dobott_text";
-            this.LB_dobott_text.Size = new System.Drawing.Size(90, 15);
+            this.LB_dobott_text.Size = new System.Drawing.Size(100, 17);
             this.LB_dobott_text.TabIndex = 1;
             this.LB_dobott_text.Text = "Dobott számok:";
             // 
             // LB_Dobott1
             // 
             this.LB_Dobott1.AutoSize = true;
-            this.LB_Dobott1.Location = new System.Drawing.Point(96, 42);
+            this.LB_Dobott1.BackColor = System.Drawing.Color.Transparent;
+            this.LB_Dobott1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LB_Dobott1.Location = new System.Drawing.Point(12, 157);
             this.LB_Dobott1.Name = "LB_Dobott1";
-            this.LB_Dobott1.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott1.Size = new System.Drawing.Size(15, 17);
             this.LB_Dobott1.TabIndex = 2;
             this.LB_Dobott1.Text = "0";
             // 
             // LB_Dobott2
             // 
             this.LB_Dobott2.AutoSize = true;
-            this.LB_Dobott2.Location = new System.Drawing.Point(115, 42);
+            this.LB_Dobott2.BackColor = System.Drawing.Color.Transparent;
+            this.LB_Dobott2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LB_Dobott2.Location = new System.Drawing.Point(33, 157);
             this.LB_Dobott2.Name = "LB_Dobott2";
-            this.LB_Dobott2.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott2.Size = new System.Drawing.Size(15, 17);
             this.LB_Dobott2.TabIndex = 3;
             this.LB_Dobott2.Text = "0";
             // 
             // LB_Dobott3
             // 
             this.LB_Dobott3.AutoSize = true;
-            this.LB_Dobott3.Location = new System.Drawing.Point(134, 42);
+            this.LB_Dobott3.BackColor = System.Drawing.Color.Transparent;
+            this.LB_Dobott3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LB_Dobott3.Location = new System.Drawing.Point(54, 157);
             this.LB_Dobott3.Name = "LB_Dobott3";
-            this.LB_Dobott3.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott3.Size = new System.Drawing.Size(15, 17);
             this.LB_Dobott3.TabIndex = 4;
             this.LB_Dobott3.Text = "0";
             // 
             // LB_Dobott4
             // 
             this.LB_Dobott4.AutoSize = true;
-            this.LB_Dobott4.Location = new System.Drawing.Point(153, 42);
+            this.LB_Dobott4.BackColor = System.Drawing.Color.Transparent;
+            this.LB_Dobott4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LB_Dobott4.Location = new System.Drawing.Point(75, 157);
             this.LB_Dobott4.Name = "LB_Dobott4";
-            this.LB_Dobott4.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott4.Size = new System.Drawing.Size(15, 17);
             this.LB_Dobott4.TabIndex = 5;
             this.LB_Dobott4.Text = "0";
             // 
             // LB_Dobott5
             // 
             this.LB_Dobott5.AutoSize = true;
-            this.LB_Dobott5.Location = new System.Drawing.Point(173, 42);
+            this.LB_Dobott5.BackColor = System.Drawing.Color.Transparent;
+            this.LB_Dobott5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LB_Dobott5.Location = new System.Drawing.Point(96, 157);
             this.LB_Dobott5.Name = "LB_Dobott5";
-            this.LB_Dobott5.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott5.Size = new System.Drawing.Size(15, 17);
             this.LB_Dobott5.TabIndex = 6;
             this.LB_Dobott5.Text = "0";
             // 
             // CHB_Dobott1
             // 
             this.CHB_Dobott1.AutoSize = true;
-            this.CHB_Dobott1.Location = new System.Drawing.Point(96, 90);
+            this.CHB_Dobott1.BackColor = System.Drawing.Color.Transparent;
+            this.CHB_Dobott1.Location = new System.Drawing.Point(13, 194);
             this.CHB_Dobott1.Name = "CHB_Dobott1";
             this.CHB_Dobott1.Size = new System.Drawing.Size(15, 14);
             this.CHB_Dobott1.TabIndex = 7;
-            this.CHB_Dobott1.UseVisualStyleBackColor = true;
+            this.CHB_Dobott1.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(96, 72);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(10, 174);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 15);
+            this.label1.Size = new System.Drawing.Size(117, 17);
             this.label1.TabIndex = 8;
             this.label1.Text = "Megtartott kockák:";
             // 
             // CHB_Dobott2
             // 
             this.CHB_Dobott2.AutoSize = true;
-            this.CHB_Dobott2.Location = new System.Drawing.Point(115, 90);
+            this.CHB_Dobott2.BackColor = System.Drawing.Color.Transparent;
+            this.CHB_Dobott2.Location = new System.Drawing.Point(33, 194);
             this.CHB_Dobott2.Name = "CHB_Dobott2";
             this.CHB_Dobott2.Size = new System.Drawing.Size(15, 14);
             this.CHB_Dobott2.TabIndex = 9;
-            this.CHB_Dobott2.UseVisualStyleBackColor = true;
+            this.CHB_Dobott2.UseVisualStyleBackColor = false;
             // 
             // CHB_Dobott3
             // 
             this.CHB_Dobott3.AutoSize = true;
-            this.CHB_Dobott3.Location = new System.Drawing.Point(134, 90);
+            this.CHB_Dobott3.BackColor = System.Drawing.Color.Transparent;
+            this.CHB_Dobott3.Location = new System.Drawing.Point(54, 194);
             this.CHB_Dobott3.Name = "CHB_Dobott3";
             this.CHB_Dobott3.Size = new System.Drawing.Size(15, 14);
             this.CHB_Dobott3.TabIndex = 10;
-            this.CHB_Dobott3.UseVisualStyleBackColor = true;
+            this.CHB_Dobott3.UseVisualStyleBackColor = false;
             // 
             // CHB_Dobott4
             // 
             this.CHB_Dobott4.AutoSize = true;
-            this.CHB_Dobott4.Location = new System.Drawing.Point(153, 90);
+            this.CHB_Dobott4.BackColor = System.Drawing.Color.Transparent;
+            this.CHB_Dobott4.Location = new System.Drawing.Point(75, 194);
             this.CHB_Dobott4.Name = "CHB_Dobott4";
             this.CHB_Dobott4.Size = new System.Drawing.Size(15, 14);
             this.CHB_Dobott4.TabIndex = 11;
-            this.CHB_Dobott4.UseVisualStyleBackColor = true;
+            this.CHB_Dobott4.UseVisualStyleBackColor = false;
             // 
             // CHB_Dobott5
             // 
             this.CHB_Dobott5.AutoSize = true;
-            this.CHB_Dobott5.Location = new System.Drawing.Point(173, 90);
+            this.CHB_Dobott5.BackColor = System.Drawing.Color.Transparent;
+            this.CHB_Dobott5.Location = new System.Drawing.Point(96, 194);
             this.CHB_Dobott5.Name = "CHB_Dobott5";
             this.CHB_Dobott5.Size = new System.Drawing.Size(15, 14);
             this.CHB_Dobott5.TabIndex = 12;
-            this.CHB_Dobott5.UseVisualStyleBackColor = true;
+            this.CHB_Dobott5.UseVisualStyleBackColor = false;
             // 
             // LB_Dobasok
             // 
             this.LB_Dobasok.AutoSize = true;
-            this.LB_Dobasok.Location = new System.Drawing.Point(12, 90);
+            this.LB_Dobasok.BackColor = System.Drawing.Color.Transparent;
+            this.LB_Dobasok.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LB_Dobasok.Location = new System.Drawing.Point(41, 90);
             this.LB_Dobasok.Name = "LB_Dobasok";
-            this.LB_Dobasok.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobasok.Size = new System.Drawing.Size(83, 20);
             this.LB_Dobasok.TabIndex = 13;
-            this.LB_Dobasok.Text = "0";
+            this.LB_Dobasok.Text = "Dobások: 0";
             // 
             // DGV_Tablazat
             // 
@@ -181,16 +212,40 @@
             this.DGV_Tablazat.AllowUserToDeleteRows = false;
             this.DGV_Tablazat.AllowUserToResizeColumns = false;
             this.DGV_Tablazat.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.MediumSeaGreen;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            this.DGV_Tablazat.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.DGV_Tablazat.BackgroundColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.MediumSeaGreen;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DGV_Tablazat.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.DGV_Tablazat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DGV_Tablazat.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Kat,
             this.Pont});
-            this.DGV_Tablazat.Location = new System.Drawing.Point(209, 12);
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.MediumSeaGreen;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DGV_Tablazat.DefaultCellStyle = dataGridViewCellStyle4;
+            this.DGV_Tablazat.EnableHeadersVisualStyles = false;
+            this.DGV_Tablazat.GridColor = System.Drawing.Color.Black;
+            this.DGV_Tablazat.Location = new System.Drawing.Point(168, 12);
             this.DGV_Tablazat.Name = "DGV_Tablazat";
             this.DGV_Tablazat.RowHeadersVisible = false;
             this.DGV_Tablazat.RowTemplate.Height = 25;
-            this.DGV_Tablazat.Size = new System.Drawing.Size(180, 375);
+            this.DGV_Tablazat.Size = new System.Drawing.Size(180, 425);
             this.DGV_Tablazat.TabIndex = 14;
+            this.DGV_Tablazat.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Kivalasztas);
             // 
             // Kat
             // 
@@ -203,16 +258,48 @@
             // Pont
             // 
             this.Pont.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.SeaGreen;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.MediumSeaGreen;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            this.Pont.DefaultCellStyle = dataGridViewCellStyle3;
             this.Pont.HeaderText = "Pontszám";
             this.Pont.Name = "Pont";
             this.Pont.ReadOnly = true;
-            this.Pont.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Pont.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // LB_Korok
+            // 
+            this.LB_Korok.AutoSize = true;
+            this.LB_Korok.BackColor = System.Drawing.Color.Transparent;
+            this.LB_Korok.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LB_Korok.Location = new System.Drawing.Point(41, 110);
+            this.LB_Korok.Name = "LB_Korok";
+            this.LB_Korok.Size = new System.Drawing.Size(69, 20);
+            this.LB_Korok.TabIndex = 15;
+            this.LB_Korok.Text = "Kör: 0/15";
+            // 
+            // LB_Vegeredmeny
+            // 
+            this.LB_Vegeredmeny.AutoSize = true;
+            this.LB_Vegeredmeny.BackColor = System.Drawing.Color.Transparent;
+            this.LB_Vegeredmeny.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LB_Vegeredmeny.Location = new System.Drawing.Point(13, 412);
+            this.LB_Vegeredmeny.Name = "LB_Vegeredmeny";
+            this.LB_Vegeredmeny.Size = new System.Drawing.Size(145, 25);
+            this.LB_Vegeredmeny.TabIndex = 16;
+            this.LB_Vegeredmeny.Text = "Végeredmény: -";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(359, 450);
+            this.Controls.Add(this.LB_Vegeredmeny);
+            this.Controls.Add(this.LB_Korok);
             this.Controls.Add(this.DGV_Tablazat);
             this.Controls.Add(this.LB_Dobasok);
             this.Controls.Add(this.CHB_Dobott5);
@@ -228,7 +315,10 @@
             this.Controls.Add(this.LB_Dobott1);
             this.Controls.Add(this.LB_dobott_text);
             this.Controls.Add(this.BTN_Dobas);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Tablazat)).EndInit();
             this.ResumeLayout(false);
@@ -253,7 +343,9 @@
         private CheckBox CHB_Dobott5;
         private Label LB_Dobasok;
         private DataGridView DGV_Tablazat;
+        private Label LB_Korok;
+        private Label LB_Vegeredmeny;
         private DataGridViewTextBoxColumn Kat;
-        private DataGridViewTextBoxColumn Pont;
+        private DataGridViewButtonColumn Pont;
     }
 }
--- Kockapoker/Form1.cs
+++ Kockapoker/Form1.cs
@@ -4,33 +4,37 @@ namespace Kockapoker
     {
         static int[] kockak = { 1, 2, 3, 4, 5 };
         static int[] kivalasztott_kockak = { 0, 0, 0, 0, 0 }; //ide mentjük el a kiválasztott kockák értékeit
-        static int dobasok = 10; //3szor lehet újradobni
+        static int dobasok = 3; //3szor lehet újradobni
         static int korok = 15; //15 kör van
+        static int[] pontszam = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
         public Form1()
         {
             InitializeComponent();
-            LB_Dobasok.Text = dobasok.ToString();
             //nem lehet lenntartani az értékeket inicializáláskór
             CHB_Dobott1.Enabled = false; 
             CHB_Dobott2.Enabled = false;
             CHB_Dobott3.Enabled = false;
             CHB_Dobott4.Enabled = false;
             CHB_Dobott5.Enabled = false;
-            string[] Kategoria = { "Egyesek", "Kettesek", "Hármasok", "Négyesek", "Ötösök", "Hatosok",
-                                   "Pár", "Terc", "Póker", "Full House", "Kis sor", "Nagy sor", "Yahtzee", "Összeg"};
+            string[] Kategoria = { "Egyesek", "Kettesek", "Hármasok", "Négyesek", "Ötösök", "Hatosok", 
+                                   "Pár", "Terc", "Póker", "Full House", "Kis sor", "Nagy sor", "Yahtzee", "Összeg",
+                                   "Felsõ sor", "Bónusz"};
             for (int i = 0; i < Kategoria.Length; i++)
             {
                 DGV_Tablazat.Rows.Add(Kategoria[i]);
             }
+            DGV_Tablazat.Enabled = false;
+            LB_Dobasok.Text = "Dobások: " + dobasok.ToString();
+            LB_Korok.Text = "Kör: " + (16-korok).ToString() + ". / 15.";
         }
 
         private void BTN_Dobas_Click(object sender, EventArgs e)
         {
-
+            DGV_Tablazat.Enabled = true;
             if (dobasok != 0)
             {
                 dobasok--;
-                LB_Dobasok.Text = dobasok.ToString();
+                LB_Dobasok.Text = "Dobások: " + dobasok.ToString();
 
                 //kockák kiválasztása:
                 if (LB_Dobott1.Text != "0")
@@ -85,68 +89,98 @@ namespace Kockapoker
             List<int> Pontok = new List<int>();
             //felsõ sor:
             //egyesek
-            Pontok.Add(darabszam[0]);
-            //Pontok.Add(kockak.Count(x => x == 1));
+            Pontok.Add(darabszam[0] * 1);
             //kettesek
-            Pontok.Add(darabszam[1]*2);
-            //Pontok.Add(kockak.Count(x => x == 2)*2);
+            Pontok.Add(darabszam[1] * 2);
             //hármasok
-            Pontok.Add(darabszam[2]*3);
-            //Pontok.Add(kockak.Count(x => x == 3)*3);
+            Pontok.Add(darabszam[2] * 3);
             //négyesek
-            Pontok.Add(darabszam[3]*4);
-            //Pontok.Add(kockak.Count(x => x == 4)*4);
+            Pontok.Add(darabszam[3] * 4);
             //ötösok
-            Pontok.Add(darabszam[4]*5);
-            //Pontok.Add(kockak.Count(x => x == 5)*5);
+            Pontok.Add(darabszam[4] * 5);
             //hatosok:
-            Pontok.Add(darabszam[5]*6);
-            //Pontok.Add(kockak.Count(x => x == 6)*6);
+            Pontok.Add(darabszam[5] * 6);
             //alsó sor:
             //pár
             Pontok.Add(darabszam.Count(x => x >= 2) >= 1 ? kockak.Sum() : 0);
-            /*Pontok.Add(kockak.Count(x => x == 1) >= 2 ? kockak.Sum() :
-                       kockak.Count(x => x == 2) >= 2 ? kockak.Sum() :
-                       kockak.Count(x => x == 3) >= 2 ? kockak.Sum() :
-                       kockak.Count(x => x == 4) >= 2 ? kockak.Sum() :
-                       kockak.Count(x => x == 5) >= 2 ? kockak.Sum() :
-                       kockak.Count(x => x == 6) >= 2 ? kockak.Sum() : 0);*/
             //terc
             Pontok.Add(darabszam.Count(x => x >= 3) >= 1 ? kockak.Sum() : 0);
-            /*Pontok.Add(kockak.Count(x => x == 1) >= 3 ? kockak.Sum() :
-                       kockak.Count(x => x == 2) >= 3 ? kockak.Sum() :
-                       kockak.Count(x => x == 3) >= 3 ? kockak.Sum() :
-                       kockak.Count(x => x == 4) >= 3 ? kockak.Sum() :
-                       kockak.Count(x => x == 5) >= 3 ? kockak.Sum() :
-                       kockak.Count(x => x == 6) >= 3 ? kockak.Sum() : 0);*/
             //póker
             Pontok.Add(darabszam.Count(x => x >= 4) >= 1 ? kockak.Sum() : 0);
-            /*Pontok.Add(kockak.Count(x => x == 1) >= 4 ? kockak.Sum() :
-                       kockak.Count(x => x == 2) >= 4 ? kockak.Sum() :
-                       kockak.Count(x => x == 3) >= 4 ? kockak.Sum() :
-                       kockak.Count(x => x == 4) >= 4 ? kockak.Sum() :
-                       kockak.Count(x => x == 5) >= 4 ? kockak.Sum() :
-                       kockak.Count(x => x == 6) >= 4 ? kockak.Sum() : 0);*/
             //full house:
-            Pontok.Add(darabszam.Count(x => x == 2) == 1 && darabszam.Count(x => x == 3)  == 1 ? 25 : 0);
+            Pontok.Add(darabszam.Count(x => x == 2) == 1 && darabszam.Count(x => x == 3) == 1 ? 25 : 0);
             //kis sor:
             int[] rendezett = { kockak[0], kockak[1], kockak[2], kockak[3], kockak[4] };
             Array.Sort(rendezett);
             string sor = rendezett[0].ToString() + rendezett[1].ToString() + rendezett[2].ToString() + rendezett[3].ToString() + rendezett[4].ToString();
-            Pontok.Add(sor.Contains("1234") || sor.Contains("2345") || sor.Contains("3456")? 30 : 0);
+            Pontok.Add(sor.Contains("1234") || sor.Contains("2345") || sor.Contains("3456") ? 30 : 0);
             //nagy sor:
             Pontok.Add(sor == "12345" || sor == "23456" ? 40 : 0);
+            //yahtzee:
+            Pontok.Add(darabszam.Count(x => x == 5) == 1 ? 50 : 0);
+            //összeg:
+            Pontok.Add(kockak.Sum());
             //kitölti a táblázatot:
             for (int i = 0; i < Pontok.Count; i++)
             {
                 DGV_Tablazat[1, i].Value = Pontok[i];
+                if (pontszam[i] != -1) DGV_Tablazat[1, i].Value = pontszam[i].ToString();
             }
             //eltárolja és továbbadja a kiválasztott cellát a "Kivalasztas" metódusnak
         }
 
-        private static void Kivalasztas(int index)
+        private void Kivalasztas(object sender, DataGridViewCellEventArgs e)
         {
             //eltárolja a kiválasztott cella pontját
+            if (e.RowIndex < 14 && e.ColumnIndex == 1)
+            {
+                if(pontszam[e.RowIndex] == -1)
+                {
+                    pontszam[e.RowIndex] = int.Parse(DGV_Tablazat[e.ColumnIndex, e.RowIndex].Value.ToString());
+                    DGV_Tablazat[e.ColumnIndex, e.RowIndex].Style.Font = new Font(DGV_Tablazat.Font, FontStyle.Bold);
+                    //alaphelyzetbe állás
+                    BTN_Dobas.Enabled = true;
+                    CHB_Dobott1.Enabled = false;
+                    CHB_Dobott2.Enabled = false;
+                    CHB_Dobott3.Enabled = false;
+                    CHB_Dobott4.Enabled = false;
+                    CHB_Dobott5.Enabled = false;
+                    CHB_Dobott1.Checked = false;
+                    CHB_Dobott2.Checked = false;
+                    CHB_Dobott3.Checked = false;
+                    CHB_Dobott4.Checked = false;
+                    CHB_Dobott5.Checked = false;
+                    for (int i = 0; i < DGV_Tablazat.Rows.Count; i++)
+                    {
+                        if (!DGV_Tablazat[1,e.RowIndex].Style.Font.Bold) DGV_Tablazat[1, i].Value = null;
+                    }
+                    DGV_Tablazat.Enabled = false;
+                    dobasok = 3;
+                    korok--;
+                    LB_Dobasok.Text = "Dobások: " + dobasok.ToString();
+                    LB_Korok.Text = "Kör: " + (16 - korok).ToString() + ". / 15.";
+                    if (korok == 1)
+                    {
+                        BTN_Dobas.Enabled = false;
+                        int felso = 0; 
+                        for (int i = 0; i < 6; i++)
+                        {
+                            felso += pontszam[i];
+                        }
+                        int bonusz = 0;
+                        if (felso > 63) bonusz = 35;
+                        int also = 0;
+                        for (int i = 6; i < pontszam.Length; i++)
+                        {
+                            also += pontszam[i];
+                        }
+                        int Vegeredmeny = felso + bonusz + also + 2;
+                        DGV_Tablazat[1, 14].Value = felso.ToString();
+                        DGV_Tablazat[1, 15].Value = bonusz.ToString();
+                        LB_Vegeredmeny.Text = "Végeredmény: " + Vegeredmeny;
+                    }
+                }
+            }
         }
     }
 }
