5d64393ed4009fa06ee2d43843298433707cdd3f
Bence Oláh <olah.bence@verebelyszki.hu>
2023-01-08 12:41:44 +0100

    Dobas elkeszitese

--- Kockapoker/Form1.Designer.cs
+++ Kockapoker/Form1.Designer.cs
@@ -28,12 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BTN_Dobas = new System.Windows.Forms.Button();
+            this.LB_dobott_text = new System.Windows.Forms.Label();
+            this.LB_Dobott1 = new System.Windows.Forms.Label();
+            this.LB_Dobott2 = new System.Windows.Forms.Label();
+            this.LB_Dobott3 = new System.Windows.Forms.Label();
+            this.LB_Dobott4 = new System.Windows.Forms.Label();
+            this.LB_Dobott5 = new System.Windows.Forms.Label();
+            this.CHB_Dobott1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CHB_Dobott2 = new System.Windows.Forms.CheckBox();
+            this.CHB_Dobott3 = new System.Windows.Forms.CheckBox();
+            this.CHB_Dobott4 = new System.Windows.Forms.CheckBox();
+            this.CHB_Dobott5 = new System.Windows.Forms.CheckBox();
+            this.LB_Dobasok = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BTN_Dobas
+            // 
+            this.BTN_Dobas.Location = new System.Drawing.Point(12, 12);
+            this.BTN_Dobas.Name = "BTN_Dobas";
+            this.BTN_Dobas.Size = new System.Drawing.Size(75, 75);
+            this.BTN_Dobas.TabIndex = 0;
+            this.BTN_Dobas.Text = "Dobás";
+            this.BTN_Dobas.UseVisualStyleBackColor = true;
+            this.BTN_Dobas.Click += new System.EventHandler(this.BTN_Dobas_Click);
+            // 
+            // LB_dobott_text
+            // 
+            this.LB_dobott_text.AutoSize = true;
+            this.LB_dobott_text.Location = new System.Drawing.Point(96, 16);
+            this.LB_dobott_text.Name = "LB_dobott_text";
+            this.LB_dobott_text.Size = new System.Drawing.Size(90, 15);
+            this.LB_dobott_text.TabIndex = 1;
+            this.LB_dobott_text.Text = "Dobott számok:";
+            // 
+            // LB_Dobott1
+            // 
+            this.LB_Dobott1.AutoSize = true;
+            this.LB_Dobott1.Location = new System.Drawing.Point(96, 42);
+            this.LB_Dobott1.Name = "LB_Dobott1";
+            this.LB_Dobott1.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott1.TabIndex = 2;
+            this.LB_Dobott1.Text = "0";
+            // 
+            // LB_Dobott2
+            // 
+            this.LB_Dobott2.AutoSize = true;
+            this.LB_Dobott2.Location = new System.Drawing.Point(115, 42);
+            this.LB_Dobott2.Name = "LB_Dobott2";
+            this.LB_Dobott2.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott2.TabIndex = 3;
+            this.LB_Dobott2.Text = "0";
+            // 
+            // LB_Dobott3
+            // 
+            this.LB_Dobott3.AutoSize = true;
+            this.LB_Dobott3.Location = new System.Drawing.Point(134, 42);
+            this.LB_Dobott3.Name = "LB_Dobott3";
+            this.LB_Dobott3.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott3.TabIndex = 4;
+            this.LB_Dobott3.Text = "0";
+            // 
+            // LB_Dobott4
+            // 
+            this.LB_Dobott4.AutoSize = true;
+            this.LB_Dobott4.Location = new System.Drawing.Point(153, 42);
+            this.LB_Dobott4.Name = "LB_Dobott4";
+            this.LB_Dobott4.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott4.TabIndex = 5;
+            this.LB_Dobott4.Text = "0";
+            // 
+            // LB_Dobott5
+            // 
+            this.LB_Dobott5.AutoSize = true;
+            this.LB_Dobott5.Location = new System.Drawing.Point(173, 42);
+            this.LB_Dobott5.Name = "LB_Dobott5";
+            this.LB_Dobott5.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobott5.TabIndex = 6;
+            this.LB_Dobott5.Text = "0";
+            // 
+            // CHB_Dobott1
+            // 
+            this.CHB_Dobott1.AutoSize = true;
+            this.CHB_Dobott1.Location = new System.Drawing.Point(96, 90);
+            this.CHB_Dobott1.Name = "CHB_Dobott1";
+            this.CHB_Dobott1.Size = new System.Drawing.Size(15, 14);
+            this.CHB_Dobott1.TabIndex = 7;
+            this.CHB_Dobott1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Megtartott kockák:";
+            // 
+            // CHB_Dobott2
+            // 
+            this.CHB_Dobott2.AutoSize = true;
+            this.CHB_Dobott2.Location = new System.Drawing.Point(115, 90);
+            this.CHB_Dobott2.Name = "CHB_Dobott2";
+            this.CHB_Dobott2.Size = new System.Drawing.Size(15, 14);
+            this.CHB_Dobott2.TabIndex = 9;
+            this.CHB_Dobott2.UseVisualStyleBackColor = true;
+            // 
+            // CHB_Dobott3
+            // 
+            this.CHB_Dobott3.AutoSize = true;
+            this.CHB_Dobott3.Location = new System.Drawing.Point(134, 90);
+            this.CHB_Dobott3.Name = "CHB_Dobott3";
+            this.CHB_Dobott3.Size = new System.Drawing.Size(15, 14);
+            this.CHB_Dobott3.TabIndex = 10;
+            this.CHB_Dobott3.UseVisualStyleBackColor = true;
+            // 
+            // CHB_Dobott4
+            // 
+            this.CHB_Dobott4.AutoSize = true;
+            this.CHB_Dobott4.Location = new System.Drawing.Point(153, 90);
+            this.CHB_Dobott4.Name = "CHB_Dobott4";
+            this.CHB_Dobott4.Size = new System.Drawing.Size(15, 14);
+            this.CHB_Dobott4.TabIndex = 11;
+            this.CHB_Dobott4.UseVisualStyleBackColor = true;
+            // 
+            // CHB_Dobott5
+            // 
+            this.CHB_Dobott5.AutoSize = true;
+            this.CHB_Dobott5.Location = new System.Drawing.Point(173, 90);
+            this.CHB_Dobott5.Name = "CHB_Dobott5";
+            this.CHB_Dobott5.Size = new System.Drawing.Size(15, 14);
+            this.CHB_Dobott5.TabIndex = 12;
+            this.CHB_Dobott5.UseVisualStyleBackColor = true;
+            // 
+            // LB_Dobasok
+            // 
+            this.LB_Dobasok.AutoSize = true;
+            this.LB_Dobasok.Location = new System.Drawing.Point(19, 126);
+            this.LB_Dobasok.Name = "LB_Dobasok";
+            this.LB_Dobasok.Size = new System.Drawing.Size(13, 15);
+            this.LB_Dobasok.TabIndex = 13;
+            this.LB_Dobasok.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LB_Dobasok);
+            this.Controls.Add(this.CHB_Dobott5);
+            this.Controls.Add(this.CHB_Dobott4);
+            this.Controls.Add(this.CHB_Dobott3);
+            this.Controls.Add(this.CHB_Dobott2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CHB_Dobott1);
+            this.Controls.Add(this.LB_Dobott5);
+            this.Controls.Add(this.LB_Dobott4);
+            this.Controls.Add(this.LB_Dobott3);
+            this.Controls.Add(this.LB_Dobott2);
+            this.Controls.Add(this.LB_Dobott1);
+            this.Controls.Add(this.LB_dobott_text);
+            this.Controls.Add(this.BTN_Dobas);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button BTN_Dobas;
+        private Label LB_dobott_text;
+        private Label LB_Dobott1;
+        private Label LB_Dobott2;
+        private Label LB_Dobott3;
+        private Label LB_Dobott4;
+        private Label LB_Dobott5;
+        private CheckBox CHB_Dobott1;
+        private Label label1;
+        private CheckBox CHB_Dobott2;
+        private CheckBox CHB_Dobott3;
+        private CheckBox CHB_Dobott4;
+        private CheckBox CHB_Dobott5;
+        private Label LB_Dobasok;
     }
 }
--- Kockapoker/Form1.cs
+++ Kockapoker/Form1.cs
@@ -2,9 +2,81 @@ namespace Kockapoker
 {
     public partial class Form1 : Form
     {
+        static int[] kockak = { 1, 2, 3, 4, 5 };
+        static int[] kivalasztott_kockak = { 0, 0, 0, 0, 0 }; //ide mentjük el a kiválasztott kockák értékeit
+        static int dobasok = 3; //3szor lehet újradobni
+        static int korok = 15; //15 kör van
         public Form1()
         {
             InitializeComponent();
+            LB_Dobasok.Text = dobasok.ToString();
+            // az értékeket a dobás elött nem lehet lenntartani
+            CHB_Dobott1.Enabled = false; 
+            CHB_Dobott2.Enabled = false;
+            CHB_Dobott3.Enabled = false;
+            CHB_Dobott4.Enabled = false;
+            CHB_Dobott5.Enabled = false;
+        }
+
+        private void BTN_Dobas_Click(object sender, EventArgs e)
+        {
+
+            if (dobasok != 0)
+            {
+                dobasok--;
+                LB_Dobasok.Text = dobasok.ToString();
+
+                //kockák kiválasztása:
+                if (LB_Dobott1.Text != "0")
+                {
+                    if (CHB_Dobott1.Checked) kivalasztott_kockak[0] = kockak[0];
+                    if (CHB_Dobott2.Checked) kivalasztott_kockak[1] = kockak[1];
+                    if (CHB_Dobott3.Checked) kivalasztott_kockak[2] = kockak[2];
+                    if (CHB_Dobott4.Checked) kivalasztott_kockak[3] = kockak[3];
+                    if (CHB_Dobott5.Checked) kivalasztott_kockak[4] = kockak[4];
+                }
+
+                //5db kocka dobása:
+                Random rnd = new Random();
+                for (int i = 0; i < 5; i++)
+                {
+                    kockak[i] = rnd.Next(1, 7);
+                }
+                if (CHB_Dobott1.Checked) kockak[0] = kivalasztott_kockak[0];
+                if (CHB_Dobott2.Checked) kockak[1] = kivalasztott_kockak[1];
+                if (CHB_Dobott3.Checked) kockak[2] = kivalasztott_kockak[2];
+                if (CHB_Dobott4.Checked) kockak[3] = kivalasztott_kockak[3];
+                if (CHB_Dobott5.Checked) kockak[4] = kivalasztott_kockak[4];
+
+                //dobott értékek kiírása:
+                LB_Dobott1.Text = kockak[0].ToString();
+                LB_Dobott2.Text = kockak[1].ToString();
+                LB_Dobott3.Text = kockak[2].ToString();
+                LB_Dobott4.Text = kockak[3].ToString();
+                LB_Dobott5.Text = kockak[4].ToString();
+
+                CHB_Dobott1.Enabled = true;
+                CHB_Dobott2.Enabled = true;
+                CHB_Dobott3.Enabled = true;
+                CHB_Dobott4.Enabled = true;
+                CHB_Dobott5.Enabled = true;
+
+
+                Kiertekeles();
+            }
+            if (dobasok == 0) BTN_Dobas.Enabled = false;
+            //elindul a kiértékelés
+        }
+
+        private static void Kiertekeles()
+        {
+            //kitölti a táblázatot
+            //eltárolja és továbbadja a kiválasztott cellát a "Kivalasztas" metódusnak
+        }
+
+        private static void Kivalasztas(int index)
+        {
+            //eltárolja a kiválasztott cella pontját
         }
     }
 }
